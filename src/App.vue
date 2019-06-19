a52a6c5108a1d331627edc113d090b471adcc0c7
Ricardo Gauto <ricardogauto11@gmail.com>
2019-06-19 16:31:50 -0300

    Scope of the main component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="principal">
+    <span>App</span>
   </div>
 </template>
 
+<script>
+export default {
+  
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+html {
+  font-size: 62.5% !important;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.principal {
+  background-color:darkred;
+  padding: 10px;
+  color: white;
+  border-radius: 5px;
+  width: 400px;
+  font-size: 3rem;
+  text-align: center
 }
 </style>
