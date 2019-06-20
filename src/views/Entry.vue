1276ee20783328ce0b4624b783b32eb230a049d2
Ricardo Gauto <ricardogauto11@gmail.com>
2019-06-20 17:34:42 -0300

    Tickets views

---
+++ src/views/Entry.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <client></client>
+  </div>
+</template>
+
+<script>
+import Client from '@/components/Client.vue'
+
+export default {
+  components: {
+    Client
+  }
+}
+</script>
+
+<style>
+
+</style>
